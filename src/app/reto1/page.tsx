347f2cd6c5f9cf5f5f85c30c0d818fecdf55979c
Sreys54 <123739656+Sreys54@users.noreply.github.com>
2025-10-08 08:32:22 -0500

    Reto 1

--- src/app/reto1/page.tsx
+++ src/app/reto1/page.tsx
@@ -1,8 +1,11 @@
+'use client';
+import ProgressBarExample from '../progressBar/ProgressBarExample';
+
 export default function Reto1() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8">
-      <h1 className="text-4xl font-bold mb-4">Reto 1</h1>
-      <p className="text-xl">Contenido del Reto 1</p>
+      <h1 className="text-4xl font-bold mb-4">Reto 1 - Progress Bar</h1>
+      <ProgressBarExample />
     </main>
   );
 }
